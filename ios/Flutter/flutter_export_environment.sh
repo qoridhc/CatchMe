f5d4da30bb23e227f1386751e4cdcaa5f6eaae93
J0a0J <105842502+J0a0J@users.noreply.github.com>
2023-04-10 12:13:12 +0900

    폰트 적용

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jiweon/development/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jiweon/development/playground/capstone4"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jiweon/development/playground/capstone4/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jiweon/development/playground/capstone4/.dart_tool/package_config.json"
