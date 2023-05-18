043e50fe108a053c6d8f1cca8f981b245bd9a211
realcold0 <realcold0@gmail.com>
2023-05-18 23:22:38 +0900

    일반 로그인 생성 및 로그인시 토큰 발행

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -2,6 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:captone4/login_platform.dart';
 import 'package:flutter_naver_login/flutter_naver_login.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+class Token{
+  final String accessToken;
+  final String type;
+
+  Token({required this.accessToken,
+  required this.type});
+
+  factory Token.fromJson(Map<String, dynamic> json){
+    return Token(accessToken: json["accessToken"], type: json["type"]);
+  }
+}
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,8 +34,10 @@ class _LoginScreenState extends State<LoginScreen> {
 
   LoginPlatform _loginPlatform = LoginPlatform.none;
 
+  final idController = TextEditingController();
+  final pwController = TextEditingController();
 
-  void signInWithNaver() async {
+  void signInWithNaver() async {   //네이버 로그인 관리
     try {
       final NaverLoginResult result = await FlutterNaverLogin.logIn();
 
@@ -41,7 +58,7 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  void signOut() async {
+  void signOut() async {  //여러 로그인 방식 관리
     switch (_loginPlatform) {
       case LoginPlatform.facebook:
         break;
@@ -67,7 +84,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     //화면 제작
 
-    return Scaffold(
+    return Scaffold(  //화면 그리기
       backgroundColor: Color(0xffEEDDD6),
       body: SafeArea(
         child: Stack(
@@ -115,7 +132,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           SizedBox(
                             width: MediaQuery.of(context).size.width * 2/3,
                             child: OutlinedButton(
-                              onPressed: buttonLoginPressed,
+                              onPressed: buttonNaverLoginPressed,  //로그인 함수 실행
                               child: Text(
                                 "Naver Login",
                                 style: TextStyle(
@@ -132,6 +149,56 @@ class _LoginScreenState extends State<LoginScreen> {
                                               BorderRadius.circular(18.0),
                                           side:
                                               BorderSide(color: Colors.red)))),
+                            ),
+                          ),
+                          SizedBox(   //ID입력
+                            width: MediaQuery.of(context).size.width * 2/3,
+                            child: OutlinedButton(
+                              onPressed: buttonLoginPressed,
+                              child: TextField(
+                                controller: idController,
+                                decoration: InputDecoration(
+                                  labelText: 'ID'
+                                ),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(18.0),
+                                          side:
+                                          BorderSide(color: Colors.red)))),
+                            ),
+                          ),
+                          SizedBox(  //password입력
+                            width: MediaQuery.of(context).size.width * 2/3,
+                            child: OutlinedButton(
+                              onPressed: buttonLoginPressed,
+                              child: TextField(
+                                controller: pwController,
+                                decoration: InputDecoration(
+                                    labelText: 'Password'
+                                ),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(18.0),
+                                          side:
+                                          BorderSide(color: Colors.red)))),
                             ),
                           ),
                           SizedBox(
@@ -169,7 +236,41 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Future<void> buttonLoginPressed() async {
+  Future<Token> buttonLoginPressed() async{
+    String userId = idController.text;
+    String password = pwController.text;
+    var url = "http://10.0.2.2:8080/api/v1/login";
+
+    print(userId);
+    print(password);
+
+    try{
+      Map data = {"userId": userId, "password" : password};
+
+      var body = json.encode(data);
+
+      final response = await http.post(Uri.parse(url),headers: {"Content-Type": "application/json"}, body: body);
+      if(response.statusCode == 200)
+        {
+          print('로그인 토큰 발행');
+          return Token.fromJson(json.decode(response.body));
+
+        }
+      else{
+        throw Exception('로그인 오류');
+
+      }
+
+    }
+    catch(e){
+      print(e);
+      rethrow;
+
+
+    }
+  }
+
+  Future<void> buttonNaverLoginPressed() async {   //로그인 눌렀을때 동작
     try {
       final NaverLoginResult res = await FlutterNaverLogin.logIn();
       setState(() {
@@ -181,7 +282,7 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  Widget _loginButton(String path, VoidCallback onTap) {
+  Widget _loginButton(String path, VoidCallback onTap) {  //네이버 로그인 버튼
     return ElevatedButton(
       onPressed: signInWithNaver,
       style: ButtonStyle(
@@ -208,4 +309,6 @@ class _LoginScreenState extends State<LoginScreen> {
       );
     }
   }
+
+
 }
