fe3510973f494fe9148c8c7e5ef15184b2b40de8
qoridhc <67674337+qoridhc@users.noreply.github.com>
2023-05-19 19:07:35 +0900

    마이페이지 수정 / 프로필 모델 생성

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightThemeDataCustom,
-      home: LoginScreen(),
+      home: RootTab(),
     );
   }
 }
