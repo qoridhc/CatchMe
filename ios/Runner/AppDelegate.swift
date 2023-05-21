abab6a4da48cd6e55f8f8064e6bd4f280e204d3b
DonghyeonKwon <93506933+DonghyeonKwon@users.noreply.github.com>
2023-05-21 18:41:23 +0900

    Edit Favorite List 3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import NaverThirdPartyLogin
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
 
-override func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     var applicationResult = false
     if (!applicationResult) {
        applicationResult = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
