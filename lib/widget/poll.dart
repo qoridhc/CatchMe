b1b30dfcebcfc2c0736109246f067a08c532588f
J0a0J <105842502+J0a0J@users.noreply.github.com>
2023-06-11 02:36:17 +0900

    투표 맞췄을 때 효과 넣음

--- lib/widget/poll.dart
+++ lib/widget/poll.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_polls/simple_polls.dart';
+import 'package:confetti/confetti.dart';
+
 // import 'package:flutter_polls/flutter_polls.dart';
 
 class PollsDemo extends StatefulWidget {
@@ -9,8 +11,37 @@ class PollsDemo extends StatefulWidget {
 
 class _PollsDemoState extends State<PollsDemo> {
   var users = ['여자 1호', '여자 2호', '여자 3호', '남자 1호', '남자 2호', '남자 3호'];
-  var guessedMafia = [];
+  var guessedJerry = [];
   late var votes;
+  final controller = ConfettiController();
+
+  @override
+  void initState() {
+    super.initState();
+    ConfettiWidget(
+      confettiController: controller,
+      colors: const [
+        Colors.green,
+        Colors.blue,
+        Colors.pink,
+        Colors.orange,
+        Colors.purple,
+      ],
+      blastDirectionality: BlastDirectionality.explosive,
+      shouldLoop: false,
+      maxBlastForce: 30,
+      minBlastForce: 5,
+      blastDirection: 3.14 / 2,
+      emissionFrequency: 0.05,
+      numberOfParticles: 50,
+      gravity: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   String user = "user@gmail.com";
   Map usersWhoVoted = {
@@ -28,80 +59,109 @@ class _PollsDemoState extends State<PollsDemo> {
         title: Text("Flutter Polls Widget Demo"),
         automaticallyImplyLeading: false,
       ),
-      body: ExpansionTile(
-        title: Text('마피아에게 투표해주세요', style: TextStyle(color: Colors.grey[700])),
+      body: Stack(
         children: [
-          SimplePollsWidget(
-            onSelection:
-                (PollFrameModel model, PollOptions? selectedOptionModel) {
-              print('전체 투표 수 : ${model.totalPolls}');
-              print('선택된 옵션 : ${selectedOptionModel!.label}');
-              print(selectedOptionModel.pollsCount);
-              votes = model.options[0].pollsCount;
-              guessedMafia.add(model.options[0].label);
-              for (int i = 1; i < users.length - 1; i++) {
-                if (model.options[i].pollsCount > votes) {
-                  votes = model.options[i].pollsCount;
-                  print(votes);
-                  guessedMafia.clear();
-                  guessedMafia.add(model.options[i].label);
-                } else if (model.options[i].pollsCount == votes) {
-                  guessedMafia.add(model.options[i].label);
-                }
-              }
-              print('가장 투표 많이 받은 사람 : $guessedMafia, 받은 표: $votes');
-            },
-            model: PollFrameModel(
-              title: Container(
-                alignment: Alignment.centerLeft,
-              ),
-              totalPolls: 3,
-              hasVoted: false,
-              editablePoll: true,
-              endTime: DateTime.now().toUtc().add(const Duration(minutes: 10)),
-              options: <PollOptions>[
-                /// Configure options here
-                PollOptions(
-                  label: users[0],
-                  pollsCount: 1,
+          ConfettiWidget(
+            confettiController: controller,
+            colors: const [
+              Colors.green,
+              Colors.blue,
+              Colors.pink,
+              Colors.orange,
+              Colors.purple,
+            ],
+            blastDirectionality: BlastDirectionality.explosive,
+            shouldLoop: false,
+            maxBlastForce: 30,
+            minBlastForce: 5,
+            blastDirection: 3.14 / 2,
+            emissionFrequency: 0.05,
+            numberOfParticles: 50,
+            gravity: 1,
+            child: ExpansionTile(
+              title: Text('제리에게 투표해주세요',
+                  style: TextStyle(color: Colors.grey[700])),
+              children: [
+                SimplePollsWidget(
+                  onSelection:
+                      (PollFrameModel model, PollOptions? selectedOptionModel) {
+                    print('전체 투표 수 : ${model.totalPolls}');
+                    print('선택된 옵션 : ${selectedOptionModel!.label}');
+                    print(selectedOptionModel.pollsCount);
+                    votes = model.options[0].pollsCount;
+                    guessedJerry.add(model.options[0].label);
+                    for (int i = 1; i < users.length - 1; i++) {
+                      if (model.options[i].pollsCount > votes) {
+                        votes = model.options[i].pollsCount;
+                        print(votes);
+                        guessedJerry.clear();
+                        guessedJerry.add(model.options[i].label);
+                      } else if (model.options[i].pollsCount == votes) {
+                        guessedJerry.add(model.options[i].label);
+                      }
+                    }
+                    print('가장 투표 많이 받은 사람 : $guessedJerry, 받은 표: $votes');
+                    if (guessedJerry[0] == selectedOptionModel.label) {
+                      print('you got it');
+                      controller.play();
+                    }
+                  },
+                  model: PollFrameModel(
+                    title: Container(
+                      alignment: Alignment.centerLeft,
+                    ),
+                    totalPolls: 3,
+                    hasVoted: false,
+                    editablePoll: true,
+                    // endTime: DateTime.now().toUtc().add(const Duration(minutes: 15)),
+                    endTime:
+                        DateTime.now().toUtc().add(const Duration(seconds: 20)),
+                    options: <PollOptions>[
+                      /// Configure options here
+                      PollOptions(
+                        label: users[0],
+                        pollsCount: 1,
 
-                  /// Polls received by that option.
-                  isSelected: false,
+                        /// Polls received by that option.
+                        isSelected: false,
 
-                  /// If poll selected.
-                  id: 1,
+                        /// If poll selected.
+                        id: 1,
 
-                  /// Option id.
-                ),
-                PollOptions(
-                  label: users[1],
-                  pollsCount: 0,
-                  isSelected: false,
-                  id: 2,
-                ),
-                PollOptions(
-                  label: users[2],
-                  pollsCount: 0,
-                  isSelected: false,
-                  id: 3,
-                ),
-                PollOptions(
-                  label: users[3],
-                  pollsCount: 0,
-                  isSelected: false,
-                  id: 4,
-                ),
-                PollOptions(
-                  label: users[4],
-                  pollsCount: 2,
-                  isSelected: false,
-                  id: 5,
-                ),
-                PollOptions(
-                  label: users[5],
-                  pollsCount: 0,
-                  isSelected: false,
-                  id: 6,
+                        /// Option id.
+                      ),
+                      PollOptions(
+                        label: users[1],
+                        pollsCount: 0,
+                        isSelected: false,
+                        id: 2,
+                      ),
+                      PollOptions(
+                        label: users[2],
+                        pollsCount: 0,
+                        isSelected: false,
+                        id: 3,
+                      ),
+                      PollOptions(
+                        label: users[3],
+                        pollsCount: 0,
+                        isSelected: false,
+                        id: 4,
+                      ),
+                      PollOptions(
+                        label: users[4],
+                        pollsCount: 2,
+                        isSelected: false,
+                        id: 5,
+                      ),
+                      PollOptions(
+                        label: users[5],
+                        pollsCount: 0,
+                        isSelected: false,
+                        id: 6,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
